c38059223c35665ecae8592689829e3ea5cf482c
evgeniy.n <evgeniy.nikanorov@doktorabc.com>
2021-12-11 18:03:35 +0200

    Try uncovered code

--- src/calc_fn.php
+++ src/calc_fn.php
@@ -98,3 +98,15 @@ function calc($arg)
 
     return $internal($arg, []);
 }
+
+function test()
+{
+    echo 'Some uncovered code';
+    $j = 10;
+    for ($i = 1; $i < 10; $i++) {
+        $j++;
+    }
+
+    return $j;
+}
+
